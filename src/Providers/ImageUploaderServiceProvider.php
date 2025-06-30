a0fe30b66f92964d8b857ca9f602fa47115ef362
alishadel2001 <alishadel2001@gmail.com>
2025-06-30 10:28:55 +0300

    edit  in service Provider

--- src/Providers/ImageUploaderServiceProvider.php
+++ src/Providers/ImageUploaderServiceProvider.php
@@ -8,12 +8,32 @@ use ImageUploader\ImageUploader;
 class ImageUploaderServiceProvider extends ServiceProvider
 {
 
+    // public function register()
+    // {
+    //     $this->app->singleton('image-uploader', function () {
+    //         return new ImageUploader();
+    //     });
+    // }
     public function register()
     {
-        $this->app->singleton('image-uploader', function () {
-            return new ImageUploader();
+        $this->mergeConfigFrom(__DIR__ . '/../config/imageupload.php', 'imageupload');
+
+        $this->app->singleton('image-uploader', function ($app) {
+            $config = $app['config']->get('imageupload');
+
+            return new ImageUploader(
+                $config['max_size'],
+                $config['allowed_mime_types'],
+                $config['disk']
+            );
         });
     }
 
-    public function boot() {}
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/imageupload.php' => config_path('imageupload.php'),
+        ], 'config');
+    }
 }
---
+++ src/config/imageupload.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'max_size' => 5120,
+    'allowed_mime_types' => [
+        'image/jpeg',
+        'image/png',
+        'image/gif',
+        'application/pdf',
+    ],
+    'disk' => 'public',
+];
